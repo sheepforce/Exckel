c2cfeeabc25ccc8deb3a050b909a843bc267ce27
Phillip Seeber <seeber@rlyeh.ipc3.uni-jena.de>
2019-01-17 16:15:06 +0100

    start with parser for gaussian TDDFT

---
+++ src/Exckel/Parser/Gaussian.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Exckel.Parser.Gaussian
+( gaussianLogTDDFTSingleState
+) where
+import Data.Text hiding (takeWhile)
+import Data.Attoparsec.Text
+import Exckel.Types
+import Text.Printf
+import Control.Applicative
+import Data.Vector hiding (takeWhile, (++))
+import Prelude hiding (takeWhile)
+
+-- | From the whole Gaussian output, parse a single excited state and skip over all other parts
+gaussianLogTDDFTSingleState :: Int -> Parser ExcState
+gaussianLogTDDFTSingleState nState' = do
+  -- starting string "Excited State   1:"
+  _ <- manyTill anyChar (string . pack $ "Excited State " ++ (printf "%3d:" nState'))
+  skipSpace
+  -- spin and symmetry label "x.xxx-A" or something like "Singlet-A"
+  _ <- (show <$> double) <|> (many1 letter)
+  _ <- char '-'
+  _ <- many1 (letter <|> digit)
+  skipSpace
+  -- energy in eV "x.xxxx eV"
+  energyElectronVolt' <- double
+  skipSpace
+  _ <- string "eV"
+  _ <- char ' '
+  -- wavelength in nm "xxxx.xx nm"
+  _ <- double
+  _ <- char ' '
+  _ <- string "nm"
+  skipSpace
+  -- oscillator strength "f=x.xxxx"
+  _ <- string $ pack "f="
+  oscillatorStrength' <- double
+  skipSpace
+  -- S**2 value "<S**2>=x.xxx"
+  _ <- string "<S**2>="
+  s2' <- double
+  endOfLine
+  -- the excitation pairs for CIS vectors
+  ciWavefunction' <- many1 $ do
+    _ <- takeWhile isHorizontalSpace
+    fromOrbI' <- decimal
+    fromOrbS' <- option Nothing (Just <$> (char 'A' <|> char 'B'))
+    _ <- takeWhile isHorizontalSpace
+    _ <- string "->"
+    _ <- takeWhile isHorizontalSpace
+    toOrbI' <- decimal
+    toOrbS' <- option Nothing (Just <$> (char 'A' <|> char 'B'))
+    _ <- takeWhile isHorizontalSpace
+    coeff' <- double
+    endOfLine
+    let (spinFrom', spinTo') = case (fromOrbS', toOrbS') of
+          (Nothing, Nothing) -> (Nothing, Nothing)
+          (Just 'A', Just 'A') -> (Just Alpha, Just Alpha)
+          (Just 'B', Just 'B') -> (Just Beta, Just Beta)
+          _ -> (Nothing, Nothing)
+    return CIDeterminant
+      { _excitationPairs =
+          [ OrbitalExcitation
+              { _fromOrb = (fromOrbI', spinFrom')
+              , _toOrb = (toOrbI', spinTo')
+              }
+          ]
+      , _coeff = coeff'
+      }
+  return ExcState
+    { _nState             = nState'
+    , _multiplicity       = Nothing
+    , _wfType             = Nothing
+    , _s2                 = Just s2'
+    , _relEnergy          = energyElectronVolt'
+    , _oscillatorStrength = oscillatorStrength'
+    , _ciWavefunction     = fromList ciWavefunction'
+    }
--- src/Exckel/Types.hs
+++ src/Exckel/Types.hs
@@ -1,21 +1,24 @@
 {-
 Defining the data types, that will be used in Exckel. Note that we will follow chemical convention
 and call the ground state in given multiplicity X0, the first excited state 1 and so on. Excited
-states are formulated in terms of Configuration Interaction and data types represent this.
+states are formulated in terms of Configuration Interaction and data types represent this. Also
+orbital counting starts from 1, no matter which program is being used.
 -}
 
 {-# LANGUAGE TemplateHaskell #-}
 module Exckel.Types
-( OrbitalExcitation(..)
+( Spin(..)
+, OrbitalExcitation(..)
 , fromOrb
 , toOrb
 , CIDeterminant(..)
 , excitationPairs
-, weight
+, coeff
 , ExcState(..)
 , nState
 , multiplicity
-, refWF
+, wfType
+, s2
 , relEnergy
 , oscillatorStrength
 , ciWavefunction
@@ -23,39 +26,41 @@ module Exckel.Types
 import           Data.Vector
 import           Lens.Micro.Platform
 
+data Spin = Alpha | Beta deriving (Eq, Show)
+
 -- | Define an excitation pair in a CI determinant
 data OrbitalExcitation = OrbitalExcitation
-  { _fromOrb :: Int
-  , _toOrb   :: Int
-  } deriving (Eq)
+  { _fromOrb :: (Int, Maybe Spin)
+  , _toOrb   :: (Int, Maybe Spin)
+  } deriving (Eq, Show)
 makeLenses ''OrbitalExcitation
 
 -- | A CI determinant is formed by a number of excitations (one for singles determinant, two
 -- | for a doubles determinant, ...) and a weight, with which it contributes to the CI wavefunction
 data CIDeterminant = CIDeterminant
   { _excitationPairs :: [OrbitalExcitation]
-  , _weight          :: Double
-  } deriving Eq
+  , _coeff           :: Double
+  } deriving (Eq, Show)
 makeLenses ''CIDeterminant
 
--- | Expectation value of the S**2 operator
-type S2 = Double
-data RefData = ClosedShell | OpenShell (Maybe S2) | RestrictedOpenShell (Maybe S2)
+data WFType = ClosedShell | OpenShell | RestrictedOpenShell deriving (Show)
 
 -- | From the reference determinant (usually DFT or HF), an excited state is formed by a CI
 -- | wavefunction. The state is characterized by its position relative to the ground state
 -- |   nState : Position/order relative to the ground state
 -- |   multiplicity : Ideal mulitplicity of the state. Together with nState gives something like S5
--- |   refWF : Type of wavefunction that has been used and possibly S**2 value of the state
+-- |   wfType : Type of wavefunction that has been used
+-- |   s2 :  Expectation value of this state for the S^2 operator
 -- |   relEnergy : Energy relative to the ground state in Hartree
 -- |   oscillatorStrength : The oscillator strength of the state
 -- |   ciWavefunction : the complete CI wavefunction, that forms this state
 data ExcState = ExcState
   { _nState             :: Int
-  , _multiplicity       :: Int
-  , _refWF              :: RefData
+  , _multiplicity       :: Maybe Int
+  , _wfType             :: Maybe WFType
+  , _s2                 :: Maybe Double
   , _relEnergy          :: Double
   , _oscillatorStrength :: Double
   , _ciWavefunction     :: Vector CIDeterminant
-  }
+  } deriving (Show)
 makeLenses ''ExcState
