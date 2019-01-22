b5cb915458ca20dbd49e77b6d93e1550db262d02
Phillip Seeber <seeber@rlyeh.ipc3.uni-jena.de>
2019-01-22 13:44:16 +0100

    Writing table continuation

--- src/Exckel/DocumentCreator.hs
+++ src/Exckel/DocumentCreator.hs
@@ -8,12 +8,14 @@ module Exckel.DocumentCreator
 (
 ) where
 import           Control.Monad.IO.Class
+import qualified Data.ByteString.Lazy   as B
+import qualified Data.Text              as T
+import qualified Data.Vector            as V
 import           Exckel.Types
 import           Lens.Micro.Platform
 import           Text.Pandoc
 import           Text.Pandoc.Builder
-import qualified Data.ByteString.Lazy as B
-import qualified Data.Text as T
+import           Text.Printf
 
 
 excitationTable :: [ExcState] -> Pandoc
@@ -32,7 +34,7 @@ excitationTable es =
        , (AlignRight, 0.1)     -- wavelength
        , (AlignRight, 0.1)     -- oscillator strength
        ]
-       -- heading of the table
+       -- heading of the tablem user "headerWith"
        [ header 1 "State"
        , header 1 "Transition"
        , header 1 "Weight / %"
@@ -40,17 +42,42 @@ excitationTable es =
        , header 1 "λ / nm"
        , header 1 "f_osc"
        ]
-       [replicate 5 (para "Hey")
-       ]
+       (tableContents es)
   where
     title :: Many Inline
     title = fromList [Str ("Excited State")]
+    tableContents :: [ExcState] -> [[Blocks]]
+    tableContents excStates =
+      map (\e -> [ para . text . show $ e ^. nState
+                 , para . text . concat . V.toList . V.map ciEntry $ (e ^. ciWavefunction)
+                 , para . text $ "Placeholder weight"
+                 , para . text . printf "%4.2F" . (* 27.11386020) $ e ^. relEnergy
+                 , para . text . show . (1239.84197386209 /) . (* 27.11386020) $ e ^. relEnergy
+                 , para . text . show $ e ^. oscillatorStrength
+                 ]
+          ) excStates
+    ciEntry :: CIDeterminant -> String
+    ciEntry ciD =
+      concatMap (\x ->
+        -- fromOrb
+        (show (x ^. fromOrb . _1)) ++
+        (case (x ^. fromOrb . _2) of
+          Just s  -> if s == Alpha then "A" else "B"
+          Nothing -> ""
+        ) ++
+        " → " ++
+        (show (x ^. toOrb . _1)) ++
+        (case (x ^. toOrb . _2) of
+          Just s  -> if s == Alpha then "A" else "B"
+          Nothing -> ""
+        ) ++
+        "\n"
+      ) $ ciD ^. excitationPairs
+
 
 ----------------------------------------------------------------------------------------------------
 -- Testing Pandoc functionality
 ----------------------------------------------------------------------------------------------------
-
-
 testDoc :: Pandoc
 testDoc = setTitle "Test Document" $ doc $
      para "Hey, this is just a pandoc test"
@@ -62,7 +89,7 @@ testPan = runIO $ do
     setUserDataDir (Just "/user/seeber/Downloads/pandoc-master/data")
     dataDir <- getUserDataDir
     case dataDir of
-      Just d -> liftIO . putStrLn $ "Data directory is: " ++ show dataDir
+      Just d  -> liftIO . putStrLn $ "Data directory is: " ++ show dataDir
       Nothing -> liftIO . putStrLn $ "Could not get a data directory"
     odtTemplate <- getDefaultTemplate "odt"
     writeODT def { writerTemplate = Just odtTemplate} testDoc
@@ -73,4 +100,4 @@ testIt es = do
   pandocWriteResult <- testPan
   case pandocWriteResult of
     Right r -> B.writeFile "/user/seeber/Test.odt" r
-    Left _ -> putStrLn "Oi, something went wrong"
+    Left _  -> putStrLn "Oi, something went wrong"
