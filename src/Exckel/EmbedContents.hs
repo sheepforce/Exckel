ee499f48930aa5ed22204d80c1be51bbad06f169
Phillip Seeber <seeber@rlyeh.ipc3.uni-jena.de>
2019-02-06 15:05:54 +0100

    embedding reference documents into sources

--- src/Exckel/EmbedContents.hs
+++ src/Exckel/EmbedContents.hs
@@ -2,15 +2,26 @@
 module Exckel.EmbedContents
 ( vmdTemplateScript
 , exckelLogo
+, defaultDocx
+, defaultODT
 ) where
 import qualified Data.ByteString       as B
 import qualified Data.ByteString.Char8 as BC
 import           Data.FileEmbed
+import           System.FilePath
 
 -- | Embed VMD template script in source code
 vmdTemplateScript :: B.ByteString
-vmdTemplateScript = $(embedFile "data/VMD.tcl")
+vmdTemplateScript = $(embedFile $ "data" ++ [pathSeparator] ++ "VMD.tcl")
 
 -- | Exckel logo
 exckelLogo :: B.ByteString
-exckelLogo = $(embedFile "data/Exckel.txt")
+exckelLogo = $(embedFile $ "data" ++ [pathSeparator] ++ "Exckel.txt")
+
+-- | Reference docx document with formatting hints for pandoc
+defaultDocx :: B.ByteString
+defaultDocx = $(embedFile $ "data" ++ [pathSeparator] ++ "DefaultRef.docx")
+
+-- | Reference odt document with formatting hints for pando
+defaultODT :: B.ByteString
+defaultODT = $(embedFile $ "data" ++ [pathSeparator] ++ "DefaultRef.odt")
