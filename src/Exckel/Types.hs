7e232561c89ec6dfa815cc253e8bce342f3fbcc3
Phillip Seeber <seeber@rlyeh.ipc3.uni-jena.de>
2019-04-16 13:40:56 +0200

    fixing print typo

--- src/Exckel/Types.hs
+++ src/Exckel/Types.hs
@@ -279,7 +279,7 @@ instance (Show CalcSoftware) where
   show a = case a of
     Gaussian{} -> "Gaussian with " ++ (show $ a ^. calcType)
     NWChem{}   -> "NWchem with " ++ (show $ a ^. calcType)
-    MRCC{}     -> "MRCC with" ++ (show $ a ^. calcType)
+    MRCC{}     -> "MRCC with " ++ (show $ a ^. calcType)
 
 
 -- | Supported output formats for the excitation summary
